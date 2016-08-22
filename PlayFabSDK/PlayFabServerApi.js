28be5aed8e97264a02ce1541982c3b2cb35608af
playfabjenkinsbot <playfabjenkinsbot@github.com>
2016-08-22 20:54:07 +0000

    Automated pf-main build from Jenkins

--- PlayFabSDK/PlayFabServerApi.js
+++ PlayFabSDK/PlayFabServerApi.js
@@ -19,7 +19,7 @@ if(!PlayFab._internalSettings) {
     PlayFab._internalSettings = {
         sessionTicket: null,
         sdkVersion: "0.27.160822",
-        buildIdentifier: "jbuild_javascriptsdk_0",
+        buildIdentifier: "jbuild_javascriptsdk_1",
         productionServerUrl: ".playfabapi.com",
         logicServerUrl: null,
 
