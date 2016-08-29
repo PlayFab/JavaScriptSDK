5143cb8cffc1d82e49702f816e116674daae5305
Paul Gilmore <paul+public@playfab.com>
2016-08-29 14:42:33 -0700

    https://api.playfab.com/releaseNotes/#160829 (#74)
    * Automated pf-main build from Jenkins
    
    * Automated build from Jenkins
    
    * https://github.com/PlayFab/SDKGenerator/commit/b216b6eb481e21fda1c1dd77144bcdaec2c9216d
    
    * Automated pf-main build from Jenkins
    
    * https://github.com/PlayFab/SDKGenerator/commit/88cfcc25e19d6f62015611f63041454be318692e
    
    * https://github.com/PlayFab/SDKGenerator/commit/19ef407e5e261ad4df59375ab998f04558872507
    
    * https://github.com/PlayFab/SDKGenerator/commit/15e56d3d20c083fa2aa03a784296e76ce7547972
    
    * Automated pf-main build from Jenkins
    
    * Automated pf-main build from Jenkins
    
    * Automated pf-main build from Jenkins
    
    * Automated pf-main build from Jenkins
    
    * Automated pf-main build from Jenkins
    
    * https://github.com/PlayFab/SDKGenerator/commit/6f6b966c8c8a7ee8377dc1fba48c8437eaec824b
    
    * Automated build from Jenkins
    
    * Automated pf-main build from Jenkins
    
    * Automated pf-main build from Jenkins
    
    * Automated build from Jenkins

--- PlayFabSDK/PlayFabAdminApi.js
+++ PlayFabSDK/PlayFabAdminApi.js
@@ -18,7 +18,7 @@ if(!PlayFab.settings) {
 if(!PlayFab._internalSettings) {
     PlayFab._internalSettings = {
         sessionTicket: null,
-        sdkVersion: "0.27.160822",
+        sdkVersion: "0.28.160829",
         buildIdentifier: "jbuild_javascriptsdk_0",
         productionServerUrl: ".playfabapi.com",
         logicServerUrl: null,
@@ -532,6 +532,12 @@ PlayFab.AdminApi = {
         PlayFab._internalSettings.ExecuteRequest(PlayFab._internalSettings.GetServerUrl() + "/Admin/ResetCharacterStatistics", request, "X-SecretKey", PlayFab.settings.developerSecretKey, callback);
     },
 
+    AddPlayerTag: function (request, callback) {
+        if (PlayFab.settings.developerSecretKey == null) throw "Must have PlayFab.settings.developerSecretKey set to call this method";
+
+        PlayFab._internalSettings.ExecuteRequest(PlayFab._internalSettings.GetServerUrl() + "/Admin/AddPlayerTag", request, "X-SecretKey", PlayFab.settings.developerSecretKey, callback);
+    },
+
     GetAllSegments: function (request, callback) {
         if (PlayFab.settings.developerSecretKey == null) throw "Must have PlayFab.settings.developerSecretKey set to call this method";
 
@@ -548,6 +554,18 @@ PlayFab.AdminApi = {
         if (PlayFab.settings.developerSecretKey == null) throw "Must have PlayFab.settings.developerSecretKey set to call this method";
 
         PlayFab._internalSettings.ExecuteRequest(PlayFab._internalSettings.GetServerUrl() + "/Admin/GetPlayersInSegment", request, "X-SecretKey", PlayFab.settings.developerSecretKey, callback);
+    },
+
+    GetPlayerTags: function (request, callback) {
+        if (PlayFab.settings.developerSecretKey == null) throw "Must have PlayFab.settings.developerSecretKey set to call this method";
+
+        PlayFab._internalSettings.ExecuteRequest(PlayFab._internalSettings.GetServerUrl() + "/Admin/GetPlayerTags", request, "X-SecretKey", PlayFab.settings.developerSecretKey, callback);
+    },
+
+    RemovePlayerTag: function (request, callback) {
+        if (PlayFab.settings.developerSecretKey == null) throw "Must have PlayFab.settings.developerSecretKey set to call this method";
+
+        PlayFab._internalSettings.ExecuteRequest(PlayFab._internalSettings.GetServerUrl() + "/Admin/RemovePlayerTag", request, "X-SecretKey", PlayFab.settings.developerSecretKey, callback);
     },
 };
 
