e4d2f3be71b29029aa4435826d232a6891b55a3f
PlayFab SDK Team <PlayFabSdkAccess@microsoft.com>
2022-05-09 22:20:57 +0000

    https://docs.microsoft.com/en-us/gaming/playfab/release-notes/#220509

--- PlayFabTestingExample/src/PlayFab/PlayFabClientApi.js
+++ PlayFabTestingExample/src/PlayFab/PlayFabClientApi.js
@@ -14,9 +14,9 @@ if(!PlayFab.settings) {
 if(!PlayFab._internalSettings) {
     PlayFab._internalSettings = {
         entityToken: null,
-        sdkVersion: "1.111.220502",
+        sdkVersion: "1.113.220509",
         requestGetParams: {
-            sdk: "JavaScriptSDK-1.111.220502"
+            sdk: "JavaScriptSDK-1.113.220509"
         },
         sessionTicket: null,
         verticalName: null, // The name of a customer vertical. This is only for customers running a private cluster. Generally you shouldn't touch this
@@ -223,8 +223,8 @@ if(!PlayFab._internalSettings) {
     }
 }
 
-PlayFab.buildIdentifier = "adobuild_javascriptsdk_118";
-PlayFab.sdkVersion = "1.111.220502";
+PlayFab.buildIdentifier = "adobuild_javascriptsdk_116";
+PlayFab.sdkVersion = "1.113.220509";
 PlayFab.GenerateErrorReport = function (error) {
     if (error == null)
         return "";
