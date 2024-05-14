560c4641631ed0504a5dc977db00e3d1d210fbb3
PlayFab SDK Team <PlayFabSdkAccess@microsoft.com>
2024-05-14 22:33:46 +0000

    https://docs.microsoft.com/en-us/gaming/playfab/release-notes/#240514

--- PlayFabSdk/src/PlayFab/PlayFabEconomyApi.js
+++ PlayFabSdk/src/PlayFab/PlayFabEconomyApi.js
@@ -14,9 +14,9 @@ if(!PlayFab.settings) {
 if(!PlayFab._internalSettings) {
     PlayFab._internalSettings = {
         entityToken: null,
-        sdkVersion: "1.171.240426",
+        sdkVersion: "1.172.240514",
         requestGetParams: {
-            sdk: "JavaScriptSDK-1.171.240426"
+            sdk: "JavaScriptSDK-1.172.240514"
         },
         sessionTicket: null,
         verticalName: null, // The name of a customer vertical. This is only for customers running a private cluster. Generally you shouldn't touch this
@@ -223,8 +223,8 @@ if(!PlayFab._internalSettings) {
     }
 }
 
-PlayFab.buildIdentifier = "adobuild_javascriptsdk_8";
-PlayFab.sdkVersion = "1.171.240426";
+PlayFab.buildIdentifier = "adobuild_javascriptsdk_117";
+PlayFab.sdkVersion = "1.172.240514";
 PlayFab.GenerateErrorReport = function (error) {
     if (error == null)
         return "";
--- PlayFabTestingExample/src/PlayFab/PlayFabServerApi.js
+++ PlayFabTestingExample/src/PlayFab/PlayFabServerApi.js
@@ -14,9 +14,9 @@ if(!PlayFab.settings) {
 if(!PlayFab._internalSettings) {
     PlayFab._internalSettings = {
         entityToken: null,
-        sdkVersion: "1.171.240426",
+        sdkVersion: "1.172.240514",
         requestGetParams: {
-            sdk: "JavaScriptSDK-1.171.240426"
+            sdk: "JavaScriptSDK-1.172.240514"
         },
         sessionTicket: null,
         verticalName: null, // The name of a customer vertical. This is only for customers running a private cluster. Generally you shouldn't touch this
@@ -223,8 +223,8 @@ if(!PlayFab._internalSettings) {
     }
 }
 
-PlayFab.buildIdentifier = "adobuild_javascriptsdk_8";
-PlayFab.sdkVersion = "1.171.240426";
+PlayFab.buildIdentifier = "adobuild_javascriptsdk_117";
+PlayFab.sdkVersion = "1.172.240514";
 PlayFab.GenerateErrorReport = function (error) {
     if (error == null)
         return "";
@@ -559,6 +559,10 @@ PlayFab.ServerApi = {
 
     LinkXboxAccount: function (request, callback, customData, extraHeaders) {
         return PlayFab._internalSettings.ExecuteRequestWrapper("/Server/LinkXboxAccount", request, "X-SecretKey", callback, customData, extraHeaders);
+    },
+
+    LoginWithPSN: function (request, callback, customData, extraHeaders) {
+        return PlayFab._internalSettings.ExecuteRequestWrapper("/Server/LoginWithPSN", request, "X-SecretKey", callback, customData, extraHeaders);
     },
 
     LoginWithServerCustomId: function (request, callback, customData, extraHeaders) {
